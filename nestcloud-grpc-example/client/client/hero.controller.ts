8c2c820dfe97caf17e08424276fafb95e7735a36
Priscilla Chen <89949235+calpc@users.noreply.github.com>
2022-06-01 16:52:34 -0700

    Add files via upload

---
+++ nestcloud-grpc-example/client/client/hero.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { GrpcClient, RpcClient, Service } from '@nestcloud/grpc';
+import { HeroService } from './interfaces/hero-service.interface';
+import { join } from 'path';
+import { ListHeroResponse } from './interfaces/hero.interface';
+
+@Controller('/heros')
+export class HeroController {
+    @RpcClient({
+        service: 'rpc-server',
+        package: 'hero',
+        protoPath: join(__dirname, './interfaces/hero-service.proto'),
+    })
+    private readonly client: GrpcClient;
+    @Service('HeroService', {
+        service: 'rpc-server',
+        package: 'hero',
+        protoPath: join(__dirname, './interfaces/hero-service.proto'),
+    })
+    private heroService: HeroService;
+
+    @Get('/:heroId')
+    async get(@Param('heroId') heroId: number): Promise<any> {
+        const data = await this.heroService.get({ id: heroId }).toPromise();
+        console.log(data);
+        return data;
+    }
+
+    @Get('/')
+    async list(): Promise<ListHeroResponse> {
+        const data = await this.heroService.list({}).toPromise();
+        console.log(data);
+        return data;
+    }
+
+    @Post(':id/heros')
+    async RandomName(): Promise<ListHeroResponse> {
+        const data = await this.heroService.list({}).toPromise();
+        console.log(data);
+        return data;
+  }
+}
---
+++ nestcloud-grpc-example/client/client/interfaces/hero.interface.ts
@@ -0,0 +1,17 @@
+export interface Hero {
+    id: number;
+    name: string;
+    ranking: number;
+}
+
+export interface GetHeroRequest {
+    id: number;
+}
+
+export interface GetHeroResponse {
+    hero: Hero;
+}
+
+export interface ListHeroResponse {
+    heros: Hero[];
+}
