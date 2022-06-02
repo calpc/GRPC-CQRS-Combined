f34fc5985252f839a84c799e2160c75714512a57
Priscilla Chen <89949235+calpc@users.noreply.github.com>
2022-06-02 15:14:21 -0700

    Add files via upload

---
+++ nest-cqrs-example/dist/heroes/commands/impl/kill-heros.command.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.KillHeroCommand = void 0;
+class KillHeroCommand {
+    constructor(fid, id) {
+        this.fid = fid;
+        this.id = id;
+    }
+}
+exports.KillHeroCommand = KillHeroCommand;
+//# sourceMappingURL=kill-heros.command.js.map
---
+++ nest-cqrs-example/dist/heroes/events/impl/hero-info.event.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HeroInfoEvent = void 0;
+class HeroInfoEvent {
+    constructor(fid, id, fname, name = "new Name") {
+        this.fid = fid;
+        this.id = id;
+        this.fname = fname;
+        this.name = name;
+    }
+}
+exports.HeroInfoEvent = HeroInfoEvent;
+//# sourceMappingURL=hero-info.event.js.map
